5d7d57f9f7e5d3a8fe1eb0354889126d1f7125c7
JAntonioLJ <chernpixel@gmail.com>
2021-08-24 20:18:38 -0500

    enterprise

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
     devise_for :users
-    resources :enterprise
+    resources :enterprises
   end
 
   root to: root_path
